9f9bdc6a7a31e951a7dc9792269d038537c753bd
shuzhiqiang <>
2026-03-11 16:39:42 +0800

    chore: 解决 eslint 噪音

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,35 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import prettierConfig from 'eslint-config-prettier';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  {
-    ignores: ['dist/**', 'node_modules/**']
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettierConfig,
-  {
-    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.node
-      }
-    },
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off'
-    }
-  },
-  {
-    files: ['test/**/*.ts'],
-    languageOptions: {
-      globals: {
-        ...globals.node
-      }
-    }
-  }
-);
+	{
+		ignores: ['**/dist/**', 'node_modules/**']
+	},
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	prettierConfig,
+	{
+		files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.node
+			}
+		},
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off'
+		}
+	},
+	{
+		files: ['test/**/*.ts'],
+		languageOptions: {
+			globals: {
+				...globals.node
+			}
+		}
+	}
+)
+
